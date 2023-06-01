04c36c275ec7c3b714c2d260b0edf7f3b0ff01fe
Beauty Barua <barua.beauty@cegep-heritage.qc.cq>
2023-06-01 12:01:19 -0700

    second commit!

---
+++ products.py
@@ -0,0 +1,56 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        if not name or price < 0 or quantity < 0:
+            raise ValueError("Invalid input. Please provide a valid name, positive price, and quantity.")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.deactivate()
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+    def buy(self, quantity):
+        if quantity > self.quantity or quantity < 0:
+            raise ValueError("Invalid quantity. Please provide a valid quantity within the available stock.")
+        total_price = self.price * quantity
+        self.quantity -= quantity
+        if self.quantity == 0:
+            self.deactivate()
+        return total_price
+
+
+def main(self):
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
+
+    bose.show()
+    mac.show()
+
+    bose.set_quantity(1000)
+    bose.show()
+
+
+if __name__ == "__main__":
+    main()
